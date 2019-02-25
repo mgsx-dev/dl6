5225c9b578b94594b00fd3f208540c7f9025cdb8
Germain AUBERT <germain.aubert@gmail.com>
2019-02-25 09:12:54 +0100

    init from template

---
+++ desktop/src/net/mgsx/dl3/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package net.mgsx.dl3;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import net.mgsx.dl3.EnergyGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new EnergyGame(), config);
+	}
+}
---
+++ html/src/net/mgsx/dl3/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package net.mgsx.dl3.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import net.mgsx.dl3.EnergyGame;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new EnergyGame();
+        }
+}
