4eccd848a6e77708dba4ccc3328fa8f893b96418
Germain AUBERT <germain.aubert@gmail.com>
2019-03-01 10:53:18 +0100

    html dist, screenshots and release

--- html/src/net/mgsx/dl3/client/HtmlLauncher.java
+++ html/src/net/mgsx/dl3/client/HtmlLauncher.java
@@ -1,19 +1,47 @@
 package net.mgsx.dl3.client;
 
 import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.gwt.GwtApplication;
 import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.google.gwt.event.logical.shared.ResizeEvent;
+import com.google.gwt.event.logical.shared.ResizeHandler;
+
 import net.mgsx.dl3.EnergyGame;
 
 public class HtmlLauncher extends GwtApplication {
 
         @Override
         public GwtApplicationConfiguration getConfig () {
-                return new GwtApplicationConfiguration(480, 320);
+                return new GwtApplicationConfiguration(EnergyGame.WIDTH, EnergyGame.HEIGHT);
         }
 
         @Override
         public ApplicationListener createApplicationListener () {
                 return new EnergyGame();
         }
+        
+        @Override
+	    public void onModuleLoad () {
+	        super.onModuleLoad();
+	        com.google.gwt.user.client.Window.addResizeHandler(new ResizeHandler() {
+	              public void onResize(ResizeEvent ev) {
+	            	  final int width = ev.getWidth();
+	            	  final int height = ev.getHeight();
+	            	  getCanvasElement().setWidth(width);
+	            	  getCanvasElement().setHeight(height);
+	            	  
+	            	  // TODO try with this to confirms there is no need for the post runnable trick
+	            	  // Gdx.graphics.setWindowedMode(width, height);
+	            	  // TODO see gwt fullscreen mode already implemented !
+	            	  
+	            	  Gdx.app.postRunnable(new Runnable() {
+						@Override
+						public void run() {
+							Gdx.app.getApplicationListener().resize(width, height);
+						}
+					});
+	              }
+	            });
+	    }
 }
